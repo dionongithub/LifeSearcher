5dda77820b6ffef6fd00d930f1a31a3825878b14
dionongithub <dion_1997@hotmail.nl>
2018-05-26 12:03:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dion/lifesearcher/StartupMain.java
@@ -0,0 +1,13 @@
+package com.example.dion.lifesearcher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartupMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup_main);
+    }
+}
