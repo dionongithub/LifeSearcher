07df8d3aa92f6203518805d137675e9c1436aba2
dionongithub <dion_1997@hotmail.nl>
2018-05-27 22:24:07 +0200

    local storage, progresbar
    Add local storage and a progresbar to see beter how far you are and to remember how far you are even if you shutdown the cellphone and restart it agan

--- app/src/main/java/com/example/dion/lifesearcher/game.java
+++ app/src/main/java/com/example/dion/lifesearcher/game.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -25,11 +27,11 @@ public class game extends AppCompatActivity{
 
     TextView text;
     TextView Title;
+    ProgressBar progressBar;
     int counter;
     int ranint;
-    File file;
-    FileOutputStream outputStream;
-    String filename = "lifsearcher";
+    String Filenamecount = "counter.txt";
+    String Filenameranint = "ranint.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,21 +46,58 @@ public class game extends AppCompatActivity{
 
         adview.loadAd(adRequest);
 
-        int max = 100000;
-        int min = 10000;
-
-        Random rand = new Random();
-        ranint = rand.nextInt(max - min + 1) + min;
-
+        progressBar = findViewById(R.id.progressbar);
         Title = findViewById(R.id.TextTitle);
         text = findViewById(R.id.Textcounter);
 
+        if(!checkfile(Filenamecount)){
+
+            counter = 0;
+
+            saveFile(Filenamecount, Integer.toString(counter));
+        } else {
+            if(readFile(Filenamecount).isEmpty()){
+
+                counter = 0;
+
+                saveFile(Filenamecount, Integer.toString(counter));
+            } else {
+
+                counter = Integer.parseInt(readFile(Filenamecount));
+            }
+        }
+
+        if(!checkfile(Filenameranint)){
+            ranint = random();
+
+            saveFile(Filenameranint, Integer.toString(ranint));
+        } else {
+            if(readFile(Filenameranint).isEmpty()){
+
+                ranint = random();
+
+                saveFile(Filenameranint, Integer.toString(ranint));
+            } else {
+
+                ranint = Integer.parseInt(readFile(Filenameranint));
+            }
+        }
+
+        progressBar.setProgress(counter);
+        progressBar.setScaleY(3f);
+        progressBar.setMax(ranint);
+
         Title.setText("You have to push the button " + ranint + " To get a life.");
+        text.setText("" + this.counter);
     }
 
     public void Looking(View v){
         counter = counter + 1;
         text.setText("" + this.counter);
+
+        progressBar.setProgress(counter);
+
+        saveFile(Filenamecount, Integer.toString(counter));
 
         if(counter >= ranint) {
             Title.setText("Congrats you have now your life back.");
@@ -68,5 +107,72 @@ public class game extends AppCompatActivity{
     public void reset(View v){
         counter = 0;
         text.setText("" + this.counter);
+        saveFile(Filenamecount, Integer.toString(counter));
+
+        ranint = random();
+        saveFile(Filenameranint, Integer.toString(ranint));
+
+        progressBar.setProgress(counter);
+        progressBar.setMax(ranint);
+
+        Title.setText("\"You have to push the button " + ranint + " To get a life.");
+    }
+
+    public int random(){
+        int max = 100000;
+        int min = 10000;
+
+        Random rand = new Random();
+        ranint = rand.nextInt(max - min + 1) + min;
+
+        return ranint;
+    }
+
+    public void saveFile(String file, String text){
+        try {
+            FileOutputStream fos = openFileOutput(file, Context.MODE_PRIVATE);
+            fos.write(text.getBytes());
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(game.this, "Can't save value to storage.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public String readFile(String file){
+        String text = "";
+
+        try {
+            FileInputStream fis = openFileInput(file);
+            int size = fis.available();
+            byte[] buffer = new byte[size];
+            fis.read(buffer);
+            fis.close();
+            text = new String(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(game.this, "Gegevens ophalen fout", Toast.LENGTH_LONG).show();
+        }
+
+        return text;
+    }
+
+    public boolean checkfile(String file){
+        String text = "";
+        boolean check = true;
+
+        try {
+            FileInputStream fis = openFileInput(file);
+            int size = fis.available();
+            byte[] buffer = new byte[size];
+            fis.read(buffer);
+            fis.close();
+            text = new String(buffer);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            check = false;
+        }
+        return check;
     }
 }
